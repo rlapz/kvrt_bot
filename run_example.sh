2ef807183e98ed64b48f92692a3f210c0cd443ec
Arthur Lapz <arthurlapz@gmail.com>
2024-06-19 22:23:28 +0700

    Update `run_example.sh`
    Add `set-webhook` and `del-webhook` options
    
    Signed-off-by: Arthur Lapz <arthurlapz@gmail.com>

--- run_example.sh
+++ run_example.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+####################################################################
 export KVRT_BOT_API_TOKEN='0000000000:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 export KVRT_BOT_API_SECRET='my-secret'
 export KVRT_BOT_LISTEN_HOST=''
@@ -9,21 +11,32 @@ export KVRT_BOT_HOOK_PATH='/bot'
 export KVRT_BOT_WORKER_THREADS_NUM=4
 export KVRT_BOT_WORKER_JOBS_MIN=8
 export KVRT_BOT_WORKER_JOBS_MAX=32
-
-
-
 ####################################################################
+
+
+set_webhook() {
+	echo "setting up webhook..."
+	curl "https://api.telegram.org/bot${KVRT_BOT_API_TOKEN}/setWebhook?url=${KVRT_BOT_HOOK_URL}${KVRT_BOT_HOOK_PATH}&secret_token=${KVRT_BOT_API_SECRET}"
+}
+
+del_webhook() {
+	echo "deleting webhook..."
+	curl "https://api.telegram.org/bot${KVRT_BOT_API_TOKEN}/setWebhook?url="
+}
+
+
 if [ -z "$*" ]; then
-    ./kvrt
+	./kvrt
 elif [ "$*" = "set-webhook" ]; then
-    echo "$*"
+	set_webhook
+elif [ "$*" = "del-webhook" ]; then
+	del_webhook
 else
-    echo "invalid argument!"
-    exit 1
+	echo "invalid argument!"
+	exit 1
 fi
 
 
 RET_VAL="$?"
-echo "return value: ${RET_VAL}"
+printf "\nreturn value: %s\n" "${RET_VAL}"
 exit $RET_VAL
-####################################################################
