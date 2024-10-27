a5f84d2172941f473ef5b3e316cbb0932edcdd80
Arthur Lapz <arthurlapz@gmail.com>
2024-10-27 20:56:49 +0700

    cmd: update
    Signed-off-by: Arthur Lapz <arthurlapz@gmail.com>

--- cmd/external.c
+++ cmd/external.c
@@ -5,7 +5,7 @@
 #include "../common.h"
 
 
-static void _spawn_child_process(Update *u, const TgMessage *msg, json_object *json, const DbCmd *dbcmd);
+static int _spawn_child_process(Update *u, const TgMessage *msg, json_object *json, const DbCmd *dbcmd);
 
 
 /*
@@ -19,16 +19,22 @@ cmd_external_exec(Update *u, const TgMessage *msg, const BotCmd *cmd, json_objec
 
 	cstr_copy_n2(buffer, LEN(buffer), cmd->name, cmd->name_len);
 	int ret = db_cmd_get(&u->db, &dbcmd, msg->chat.id, buffer);
-	if (ret <= 0)
-		return 1;
+	if (ret < 0)
+		goto err0;
 
-	if (dbcmd.is_enable == 0)
-		return 1;
+	if ((ret == 0) && (dbcmd.is_enable == 0))
+		return 0;
 
 	if ((dbcmd.is_admin_only != 0) && (common_privileges_check(u, msg) < 0))
 		return 1;
 
-	_spawn_child_process(u, msg, json, &dbcmd);
+	if (_spawn_child_process(u, msg, json, &dbcmd) < 0)
+		goto err0;
+
+	return 1;
+
+err0:
+	common_send_text_plain(u, msg, "Failed to execute external command");
 	return 1;
 }
 
@@ -43,7 +49,7 @@ cmd_external_list(Update *u)
 /*
  * Private
  */
-static void
+static int
 _spawn_child_process(Update *u, const TgMessage *msg, json_object *json, const DbCmd *dbcmd)
 {
 	/* file name + NULL */
@@ -56,18 +62,18 @@ _spawn_child_process(Update *u, const TgMessage *msg, json_object *json, const D
 	if (args & DB_CMD_ARG_TYPE_RAW)
 		argv[i++] = (char *)json_object_to_json_string_ext(json, 0);
 
-	char chat_id[16];
+	char chat_id[64];
 	if (args & DB_CMD_ARG_TYPE_CHAT_ID) {
 		if (snprintf(chat_id, LEN(chat_id), "%"PRIi64, msg->chat.id) < 0)
-			return;
+			return -1;
 
 		argv[i++] = chat_id;
 	}
 
-	char user_id[16];
+	char user_id[64];
 	if (args & DB_CMD_ARG_TYPE_USER_ID) {
 		if (snprintf(user_id, LEN(user_id), "%"PRIi64, msg->from->id) < 0)
-			return;
+			return -1;
 
 		argv[i++] = user_id;
 	}
@@ -75,6 +81,10 @@ _spawn_child_process(Update *u, const TgMessage *msg, json_object *json, const D
 	if (args & DB_CMD_ARG_TYPE_TEXT)
 		argv[i] = (char *)msg->text.text;
 
-	if (chld_spawn(u->chld, dbcmd->file, argv) < 0)
+	if (chld_spawn(u->chld, dbcmd->file, argv) < 0) {
 		log_err(errno, "external: _spawn_child_process: chld_spawn: %s", dbcmd->file);
+		return -1;
+	}
+
+	return 0;
 }
