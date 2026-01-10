daf9de3e192ad588c817a52966d84580ec7c324d
Arthur Lapz <arthurlapz@gmail.com>
2026-01-10 23:53:41 +0700

    sched: try again
    Signed-off-by: Arthur Lapz <arthurlapz@gmail.com>

--- src/sched.c
+++ src/sched.c
@@ -129,24 +129,37 @@ static void
 _run_task(void *ctx, void *udata)
 {
 	ModelSchedMessage *const msg = (ModelSchedMessage *)ctx;
-	switch (msg->type) {
-	case MODEL_SCHED_MESSAGE_TYPE_SEND:
-		send_text_format(&(TgMessage){
-			.chat = (TgChat) { .id = msg->chat_id },
-			.id = msg->message_id,
-			.from = &(TgUser) { .id = msg->user_id },
-		}, NULL, "%s", msg->value);
-		break;
-	case MODEL_SCHED_MESSAGE_TYPE_DELETE:
-		delete_message(&(TgMessage){
-			.chat = (TgChat) { .id = msg->chat_id },
-			.id = msg->message_id,
-		});
-		break;
-	default:
-		LOG_ERRN("sched", "%s", "invalid task type");
+
+	int count = 3;
+	while (count--) {
+		int ret;
+		switch (msg->type) {
+		case MODEL_SCHED_MESSAGE_TYPE_SEND:
+			ret = send_text_format(&(TgMessage){
+				.chat = (TgChat) { .id = msg->chat_id },
+				.id = msg->message_id,
+				.from = &(TgUser) { .id = msg->user_id },
+			}, NULL, "%s", msg->value);
+			break;
+		case MODEL_SCHED_MESSAGE_TYPE_DELETE:
+			ret = delete_message(&(TgMessage){
+				.chat = (TgChat) { .id = msg->chat_id },
+				.id = msg->message_id,
+			});
+			break;
+		default:
+			LOG_ERRN("sched", "%s", "invalid task type");
+			ret = 0;
+			break;
+		}
+
+		// error occured, try again
+		if (ret < 0)
+			continue;
+		
 		break;
 	}
+
 
 	free(msg);
 	(void)udata;
